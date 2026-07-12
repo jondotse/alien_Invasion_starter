27d73c5e7179794e8160a35316f91677c8be48e8
JonD <sethdotse05@gmail.com>
2026-07-12 19:54:39 -0400

    Added events for ship moving left and right, built in boundaries

--- alien_invasion.py
+++ alien_invasion.py
@@ -23,7 +23,7 @@ class AlienInvasion:
         # Game loop
         while self.running:
             self._check_events()
-
+            self.ship.update()
             self._update_screen()
             self.clock.tick(self.settings.FPS) 
 
@@ -37,7 +37,27 @@ class AlienInvasion:
             if event.type == pygame.QUIT:
                 self.running = False
                 pygame.quit()
-                sys.exit()       
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._checK_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._checK_keyup_events(event)
+
+    def _checK_keyup_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+
+    def _checK_keydown_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+
 
 
 if __name__ == '__main__':
