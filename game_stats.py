472482feb55390349d6e23b4301136cca52458d9
JonD <sethdotse05@gmail.com>
2026-07-26 15:55:08 -0400

    P3 -  fleshed out game stats to update the hi score and save and read from a file

--- game_stats.py
+++ game_stats.py
@@ -1,7 +1,11 @@
+# from pathlib import Path
+import json
+
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from alien_invasion import AlienInvasion
+
 
 class GameStats():
 
@@ -9,7 +13,31 @@ class GameStats():
         self.game = game
         self.settings = game.settings
         self.max_score = 0
+        self.init_saved_scores()
         self.reset_stats()
+
+    def init_saved_scores(self):
+        self.path = self.settings.scores_file 
+        if self.path.exists() and self.path.stat.__sizeof__()> 80:
+            contents = self.path.read_text()
+            if not contents:
+                print('file empty')
+            scores = json.loads(contents)
+            self.hi_score = scores.get('hi_score', 0)
+        else:
+            self.hi_score = 0
+            self.save_scores()
+            # save the file
+
+    def save_scores(self):
+        scores = {
+            'hi_score': self.hi_score
+        }
+        contents = json.dumps(scores, indent=4)
+        try:
+            self.path.write_text(contents)
+        except FileNotFoundError as e:
+            print(f'File Not Found: {e}')
 
     def reset_stats(self):
         self.ships_left = self.settings.starting_ship_count
@@ -22,12 +50,18 @@ class GameStats():
         self._update_score(collisions)
         # update max_score
         self._update_max_score()
+
         # update hi_score
+        self._update_hi_score()
 
     def _update_max_score(self):
         if self.score > self.max_score:
             self.max_score = self.score
             # print(f'Max: {self.max_score}')
+    def _update_hi_score(self):
+        if self.score > self.hi:
+            self.hi_score = self.score
+            # print(f'Hi: {self.max_score}')
 
     def _update_score(self, collisions):
         for alien in collisions.values():
